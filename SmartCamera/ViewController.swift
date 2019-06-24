c082af133581ecbe5ed63e090d45399ceac130cd
Abhinav Mulagada <abhinav.abhi96@gmail.com>
2019-06-24 12:13:49 -0700

    Initial Commit

---
+++ SmartCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartCamera
+//
+//  Created by Abhinav Mulagada on 6/24/19.
+//  Copyright © 2019 Abhinav Mulagada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
