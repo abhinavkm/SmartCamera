c0c987f3cef18e11c9a61c823134cd3b6d507c8d
Abhinav Mulagada <abhinav.abhi96@gmail.com>
2019-06-25 12:44:26 -0700

    Initial commit

--- SmartCamera/ViewController.swift
+++ SmartCamera/ViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let captureSession = AVCaptureSession()
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+        captureSession.addInput(input)
+        
+        captureSession.startRunning()
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(previewLayer)
+        previewLayer.frame = view.frame
     }
 
 
